07eae9f9b284875b771462b942bf58a2eb7b4d8c
Clinton Hipple <clintondhipple@hotmail.com>
2020-10-09 17:40:14 -0500

    form input retrieval complete

--- CalcFunc.js
+++ CalcFunc.js
@@ -1,1 +1,26 @@
-console.log('hello');
+$(document).ready(clickWait);
+
+const employeeInfo = [];
+let totalCost =0; 
+
+function clickWait(){
+    console.log('1');
+  $('.entryButton').on('click', entryGet);
+}
+
+function entryGet(){
+   let fName = $('.fName').val();
+   let lName = $('.lName').val();
+   let id = $('.id').val();
+   let title = $('.title').val();
+   let anSalary = $('.anSalary').val();
+   const employee = {
+       FirstName: fName, 
+       LastName: lName, 
+       ID: id,
+       Title: title,
+       AnnualSalary: anSalary
+    };
+    employeeInfo.push (employee);
+}
+
