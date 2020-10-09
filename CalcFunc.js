8885b47cc714b7d1ee8ffe4e17ba0bf4d0660f19
Clinton Hipple <clintondhipple@hotmail.com>
2020-10-09 18:46:35 -0500

    form validation complete

--- CalcFunc.js
+++ CalcFunc.js
@@ -4,9 +4,31 @@ const employeeInfo = [];
 let totalCost =0; 
 
 function clickWait(){
-    console.log('1');
-  $('.entryButton').on('click', entryGet);
+  $('.entryButton').on('click', checkForm);
 }
+
+function checkForm() {
+    let firstName= $('.fName').val();
+    firstName = String(firstName);
+    firstName = firstName.trim();
+    let lastName = $('.lName').val();
+    lastName = String(lastName);
+    lastName = lastName.trim();
+    let idCheck = $('.id').val();
+    idCheck = String(idCheck);
+    idCheck = idCheck.trim();
+    let jobTitle = $('.title').val();
+    jobTitle = String(jobTitle);
+    jobTitle = jobTitle.trim();
+    let aSalary = $('.anSalary').val();
+    aSalary = String(aSalary);
+    aSalary = aSalary.trim();
+    if (firstName.length > 0 && lastName.length > 0 &&
+        idCheck.length > 0 && jobTitle.length > 0 && aSalary.length > 0) {
+      entryGet();
+    }
+    else alert('Please complete the form');
+  }
 
 function entryGet(){
    let fName = $('.fName').val();
@@ -14,13 +36,16 @@ function entryGet(){
    let id = $('.id').val();
    let title = $('.title').val();
    let anSalary = $('.anSalary').val();
+   anSalary = Number(anSalary);
    const employee = {
        FirstName: fName, 
        LastName: lName, 
        ID: id,
        Title: title,
-       AnnualSalary: anSalary
+       AnnualSalary: anSalary,
     };
+    $('.form').val('');
     employeeInfo.push (employee);
+    console.log(employeeInfo[0]);
 }
 
