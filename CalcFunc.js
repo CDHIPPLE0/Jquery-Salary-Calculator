079a3b12f9a3d68f24e26b4dacdb6d6533e3598b
Clinton Hipple <clintondhipple@hotmail.com>
2020-10-10 16:37:09 -0500

    implemented all required features

--- CalcFunc.js
+++ CalcFunc.js
@@ -1,10 +1,11 @@
 $(document).ready(clickWait);
 
-const employeeInfo = [];
+const employeeList = [];
 let totalCost =0; 
 
 function clickWait(){
   $('.entryButton').on('click', checkForm);
+  $('.addEmployee').on('click','.delete', removeAsk)
 }
 
 function checkForm() {
@@ -20,6 +21,7 @@ function checkForm() {
     let jobTitle = $('.title').val();
     jobTitle = String(jobTitle);
     jobTitle = jobTitle.trim();
+    let aSalary = $('.anSalary').val();
     if (firstName.length > 0 && lastName.length > 0 &&
         idCheck.length > 0 && jobTitle.length > 0 && aSalary.length > 0) {
       entryGet();
@@ -33,6 +35,7 @@ function entryGet(){
    let id = $('.id').val();
    let title = $('.title').val();
    let anSalary = $('.anSalary').val();
+   let isSelected = false;
    anSalary = Number(anSalary);  // changes salary back to a number
    const employee = {
        FirstName: fName, 
@@ -40,12 +43,47 @@ function entryGet(){
        ID: id,
        Title: title,
        AnnualSalary: anSalary,
+       IsSelected: isSelected,
     };
     $('.form').val('');
-    employeeInfo.push (employee);
-
+    employeeList.push (employee);
+    iterateAndDisplayProps();
 }
 
-function cycleProps(){
+function iterateAndDisplayProps(){
+  $('.addEmployee').empty();
+  let totalCost = 0;
+    for(i=0; i < employeeList.length; i++){
+      const index = employeeList[i];
+      totalCost += employeeList[i].AnnualSalary;
+       $('.addEmployee').append(
+         `<tr>
+           <td>${index.FirstName}</td>
+           <td>${index.LastName}</td>
+           <td>${index.ID}</td>
+           <td>${index.Title}</td>
+           <td>${index.AnnualSalary}</td>
+           <td><button class ='delete' type='submit' data-index="${i}">Delete</button></td>
+         </tr>`
+       )
+    }
+    $('.total').text(totalCost);
+}
 
+function removeAsk(){
+ const index = $(this).data('index');
+ employeeList[index].IsSelected = true;
+  $(this).parent().parent().remove();
+  removeDo();
+}
+
+function removeDo(){
+  let totalCost = 0;
+  for(i=0; i < employeeList.length; i++){
+    const index = employeeList[i];
+    if(index.IsSelected){
+      employeeList.splice(index, 1);
+    }
+ }
+iterateAndDisplayProps();
 }
